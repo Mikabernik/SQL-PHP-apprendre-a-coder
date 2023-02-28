a0d79fac5e5b2bd59ee5256918d2758def7d9791
mikhael rihani <mikabernik@gmail.com>
2023-02-28 09:56:07 +0100

    SQL-PHP BURGER CODE

---
+++ burger code/admin/delete.php
@@ -0,0 +1,27 @@
+<?php
+    class Database{
+
+       private static $dbHost = "localhost";
+       private static $dbName = "burger_code";
+       private static $dbUser = "root";
+       private static $dbUserPassword = "";
+       private static $connection = null;
+       
+       public static function connect(){
+             try{
+                    self::$connection = new PDO ("mysqli : host= ".self::$dbHost.";dbname= ".self::$dbName,self::$dbUser,self::$dbUserPassword);
+                }
+            catch(PDOException $e){
+                    die ($e->getMessage());
+                }
+            return self::$connection;
+        }
+
+        function disconnect(){
+            self::$connection = null;
+        }
+    }
+   
+   Database :: connect();
+
+?>
